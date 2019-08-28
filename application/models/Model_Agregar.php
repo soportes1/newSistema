843121a1fbd549419cb6f6a5f6377acd0a686f07
soportes1 <soporte1@especialistashosting.com>
2019-08-28 14:08:16 -0500

    cambio-model

--- application/models/Model_Agregar.php
+++ application/models/Model_Agregar.php
@@ -2,13 +2,22 @@
 
 class Model_Agregar extends CI_Model {
 
-	public function agregar($user,$pass)
-	{
-		$array = array("idAlumno"=>0,
-			"nombreAlumno"=>$user,
-			"password"=>$pass
-		);
-		$this->db->insert("alumno",$array);
-		return $this->db->affected_rows();
-	}
+	public function agregar($user,$name,$email,$pass)
+    {
+       $array = array("idAlumno"=>0,
+          "usuario"=>$user,
+          "nombreAlumno"=>$name,
+          "email"=>$email,
+          "password"=>$pass
+      );
+       $this->db->insert("alumno",$array);
+       return $this->db->affected_rows();
+   }
+
+   public function consulta($user,$pass){
+    $this->db->where('nombreAlumno',$user );
+    $this->db->where('password',$pass );
+    $this->db->get('alumno');
+    print_r($this->db->last_query());    
+}
 }
