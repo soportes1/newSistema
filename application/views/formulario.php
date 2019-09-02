522ec10d26d4d3c143394517b8111ce6cbb0090d
soportes1 <soporte1@especialistashosting.com>
2019-09-02 17:09:29 -0500

    cambio-style

--- application/views/formulario.php
+++ application/views/formulario.php
@@ -1,29 +1,14 @@
-<!--<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>rec/fonts/style.css">
-<script type="text/javascript" src="<?php echo base_url();?>rec/js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>rec/js/formato.js"></script>
--->
-<h1>Subir un archivo</h1>
-<!--
-<fieldset style="width: 100px;">
-	<br>
-	<form action="subir_video_controller/SubirVideo" method="post" enctype="multipart/form-data">
-		<input type="file" name="archivo"/>
-		<br></br>
-		<input type="submit" value="Subir"/>
-	</form>
-</fieldset>-->
+<form id="upload" method="post" action="upload.php" enctype="multipart/form-data">
+	<div id="drop">
+		Arrastra aquí
 
-	<form id="upload" method="post" action="upload.php" enctype="multipart/form-data">
-			<div id="drop">
-				Drop Here
+		<a>Buscar:</a>
+		<input type="file" name="upl" multiple />
+	</div>
 
-				<a>Browse</a>
-				<input type="file" name="upl" multiple />
-			</div>
+	<ul>
 
-			<ul>
-				<!-- The file uploads will be shown here -->
-			</ul>
+	</ul>
 
-		</form>
+</form>
 
