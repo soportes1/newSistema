f0c92de780f95d6237c6eb644acbc5191683251f
soportes1 <soporte1@especialistashosting.com>
2019-09-03 13:33:49 -0500

    cursos

--- application/views/ViewCursosVideos.php
+++ application/views/ViewCursosVideos.php
@@ -1,6 +1,6 @@
 <div class="content">
     <div class="container">
-       <div class="row">
+     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb40 text-center">
             <?php 
             $query = $this->db->query("SELECT * FROM cursos");
@@ -13,18 +13,18 @@
           <tbody id="mySearch">
               <tr>
                 <td><div id="mySearch" class="row">
-                   <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                 <div class="col-xl-5 ">
                     <div class="team-block">
-                        <div class="team-img">
+                        <div class="team-img" style="display: inline-block;">
                             <img src="http://mouse.latercera.com/wp-content/uploads/2017/06/4-900x600.jpg" alt="video no cargado">
-                            <div  class="team-content">
+                            <div  class="team-content" >
                                 <h4 class="text-white mb0"><?php echo $row['nombreCurso']; ?></h4>
                             </div>
                             <div class="overlay">
                                 <div class="text">
                                     <h4 class="mb0 text-white"><?php echo $row['nombreCurso']; ?></h4>
                                     <p><?php echo $row['descripcion']; ?></p>
-                                    <form >
+                                    <form>
                                       <div class="d-flex flex-row">
                                         <div class="p-2">
                                             <input id="radio1" type="radio" name="estrellas" value="5">
