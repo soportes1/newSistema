f14f64ac1a930f070a3a1fb006c3d32ed2e0524b
ehweb <soporte2@especialistashosting.com>
2019-09-04 18:58:39 -0500

    Update ViewCursosVideos.php

--- application/views/ViewCursosVideos.php
+++ application/views/ViewCursosVideos.php
@@ -58,3 +58,53 @@
 </div>
 
 
+
+    <div class="col-md-10 offset-md-1">
+        <div class="row pb-row">
+            <div class="col-md-3 pb-video">
+                <iframe class="pb-video-frame" width="100%" height="230" src="https://www.youtube.com/embed/K68UrdUOr2Y?list=RDzuAcaBkcYGE?ecver=1" 
+                https://www.youtube.com/watch?v=K68UrdUOr2Y&feature=youtu.be
+                frameborder="0" allowfullscreen></iframe>
+                <label class="form-control label-warning text-xs-center">Claydee - Alena</label>
+            </div>
+            
+        </div>
+        <div class="row pb-row">
+            <div class="col-md-3 pb-video">
+                <iframe class="pb-video-frame" width="100%" height="230" src="https://www.youtube.com/watch?v=fD2UExUhq-s?ecver=1" frameborder="0" allowfullscreen></iframe>
+                <label class="form-control label-warning text-xs-center">F.O. and Peeva - Lichnata</label>
+            </div>
+        </div>
+    </div>
+
+
+<style>
+    .pb-video-container {
+        padding-top: 20px;
+        background: #bdc3c7;
+        font-family: Lato;
+    }
+
+    .pb-video {
+        border: 1px solid #e6e6e6;
+        padding: 5px;
+    }
+
+        .pb-video:hover {
+            background: #2c3e50;
+        }
+
+    .pb-video-frame {
+        transition: width 2s, height 2s;
+    }
+
+        .pb-video-frame:hover {
+            height: 300px;
+        }
+
+    .pb-row {
+        margin-bottom: 10px;
+    }
+</style>
+
+
