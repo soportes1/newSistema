5027d6090cfd27ea2e6ced003fa322625851734f
soportes1 <soporte1@especialistashosting.com>
2019-09-20 12:14:44 -0500

    Revert "Merge branch 'master' of https://github.com/soportes1/newSistema"
    This reverts commit 48668ce635892a6df7888ae4fb1d26de9137de33, reversing
    changes made to b60e23207130bf8705da3dd671363885f83a5a8e.

--- application/views/ViewCursosVideos.php
+++ application/views/ViewCursosVideos.php
@@ -1,57 +1,62 @@
-
-<div class="padd">       
-    <div class="row">
-      <?php 
-      foreach ($query as $row)
-        {  ?>
-          <table>
-            <tbody id="mySearch">
-              <tr>
-                <td>
-                  <div id="mySearch" class="col-sm">
-                    <div class="card">
-                      <div class="poster">
-                        <img src="http://mouse.latercera.com/wp-content/uploads/2017/06/4-900x600.jpg">
-                      </div>
-                      <div class="details">
-                        <h2><?php echo $row['nombreCurso']; ?><br></h2>
-                        <!-- -->
-                        <div class="rating">
-                          <i class="fa fa-star" aria-hidden="true"></i>
-                          <i class="fa fa-star" aria-hidden="true"></i>
-                          <i class="fa fa-star" aria-hidden="true"></i>
-                          <i class="fa fa-star" aria-hidden="true"></i>
-                          <i class="fa fa-star-o" aria-hidden="true"></i>
-                          <span>4/5</span>
+<div class="content">
+  <div class="container">
+    <?php 
+    foreach ($query as $row)
+      {  ?>
+        <table>
+          <tbody id="mySearch">
+            <tr>
+              <td>
+                <div id="mySearch" class="row">
+                  <div class="col-xl-5 col-lg-5 ">
+                    <div class="team-block">
+                      <div class="team-img">
+                        <img src="http://mouse.latercera.com/wp-content/uploads/2017/06/4-900x600.jpg" alt="video no cargado">
+                        <div  class="team-content">
+                          <h4 class="text-white mb0"><?php echo $row['nombreCurso']; ?></h4>
                         </div>
-                        <div class="tags">
-                          <a name="idCurso" id="<?php echo $row['idCurso']; ?>" href="<?php echo base_url('detalle_curso'); ?>">
-                          <span class="romance">Ver detalles</span>
-                        </a>
-                        </div>
-                        <div class="info">
-                          <p>
-                            <?php echo $row['descripcion']; ?>
-                          </p>
-                        </div>
-                        <div class="star">
-                          <h4>Participantes</h4>
-                          <ul>
-                            <li><img src="https://timedotcom.files.wordpress.com/2015/03/460517204.jpg"></li>
-                            <li><img src="https://observatoriog.bol.uol.com.br/wordpress/wp-content/uploads/2017/04/Gaston-Beauty-and-the-BEast.jpg"></li>
-                            <li><img src="http://www.1023jack.com/wp-content/uploads/2016/12/BEAUTY-AND-THE-BEAST-Extended-TV-Spot-1-Christmas-Celebration-2017-Emma-Watson-Disney-Movie-HD.jpeg"></li>
-                            <li><img src="https://timedotcom.files.wordpress.com/2015/03/460517204.jpg"></li>
-                          </ul>
+                        <div class="overlay">
+                          <div class="text">
+                            <a name="idCurso" id="<?php echo $row['idCurso']; ?>" href="<?php echo base_url('detalle_curso'); ?>">
+                              <h4 class="mb0 text-white"><?php echo $row['nombreCurso']; ?></h4>
+                            </a>
+                            <p><?php echo $row['descripcion']; ?></p>
+                            <form >
+                              <div class="d-flex flex-row">
+                                <div class="p-2">
+                                  <input id="radio1" type="radio" name="estrellas" value="5">
+                                  <label for="radio1">★</label>
+                                </div>
+                                <div class="p-2">
+                                  <input id="radio2" type="radio" name="estrellas" value="4">
+                                  <label for="radio2">★</label>
+                                </div>
+                                <div class="p-2">
+                                  <input id="radio3" type="radio" name="estrellas" value="3">
+                                  <label for="radio3">★</label>
+                                </div>
+                                <div class="p-2">
+                                  <input id="radio4" type="radio" name="estrellas" value="2">
+                                  <label for="radio4">★</label>
+                                </div>
+                                <div class="p-2">
+                                  <input id="radio5" type="radio" name="estrellas" value="1">
+                                  <label for="radio5">★</label>
+                                </div>
+                              </div>
+                            </form>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <br>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        <?php } ?>
-      </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      <?php } ?>
     </div>
+  </div>
+
 
