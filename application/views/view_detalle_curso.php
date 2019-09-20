dafe71bf85400265c5e3ed10a1ee4d3be0ddbc7d
ehweb <soporte2@especialistashosting.com>
2019-09-20 12:20:49 -0500

    Revert "Merge branch 'master' of https://github.com/soportes1/newSistema"
    This reverts commit 48668ce635892a6df7888ae4fb1d26de9137de33, reversing
    changes made to b60e23207130bf8705da3dd671363885f83a5a8e.

--- application/views/view_detalle_curso.php
+++ application/views/view_detalle_curso.php
@@ -4,7 +4,7 @@
     <?php 
     foreach ($detalle as $row)
       {  ?>
-        <div class="col-md-10 offset-md-1">
+      	<div class="col-md-10 offset-md-1">
         <div class="row pb-row">
             <div class="col-md-6 pb-video aling-center">
                 <iframe class="pb-video-frame" width="100%" height="230" src="https://www.youtube.com/embed/K68UrdUOr2Y?list=RDzuAcaBkcYGE?ecver=1" 
