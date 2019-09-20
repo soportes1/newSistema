aae132183f2c3185e58631bb1ef661517edf304e
ehweb <soporte2@especialistashosting.com>
2019-09-20 12:30:51 -0500

    Update ViewCursosVideos.php

--- application/views/ViewCursosVideos.php
+++ application/views/ViewCursosVideos.php
@@ -56,23 +56,6 @@
     </div>
 
 
-    <div class="col-md-10 offset-md-1">
-        <div class="row pb-row">
-            <div class="col-md-3 pb-video">
-                <iframe class="pb-video-frame" width="100%" height="230" src="https://www.youtube.com/embed/K68UrdUOr2Y?list=RDzuAcaBkcYGE?ecver=1" 
-                https://www.youtube.com/watch?v=K68UrdUOr2Y&feature=youtu.be
-                frameborder="0" allowfullscreen></iframe>
-                <label class="form-control label-warning text-xs-center">Claydee - Alena</label>
-            </div>
-            
-        </div>
-        <div class="row pb-row">
-            <div class="col-md-3 pb-video">
-                <iframe class="pb-video-frame" width="100%" height="230" src="https://www.youtube.com/watch?v=fD2UExUhq-s?ecver=1" frameborder="0" allowfullscreen></iframe>
-                <label class="form-control label-warning text-xs-center">F.O. and Peeva - Lichnata</label>
-            </div>
-        </div>
-    </div>
 
 
 <style>
