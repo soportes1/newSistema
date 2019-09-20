40ba891bc1611615bae718120f72b8cceffdb096
ehweb <soporte2@especialistashosting.com>
2019-09-20 12:20:59 -0500

    Revert "pag"
    This reverts commit b60e23207130bf8705da3dd671363885f83a5a8e.

--- application/models/Modelo_paginador.php
+++ application/models/Modelo_paginador.php
@@ -2,24 +2,17 @@
 class Modelo_paginador extends CI_Model{
     function __construct()
     {
-       
+        // Call the Model constructor
         parent::__construct();
     }
 
+    //fetch department details from database
     function listar_videos($limit, $start)
     {
         $sql = 'select * from cursos  limit ' . $start . ', ' . $limit;
         $query = $this->db->query($sql);
         return $query->result();
     }
-
-
-   function contar_videos(){
-            return $this->db->count_all("cursos");
-
-    }
-
-
 }
 ?>
 
